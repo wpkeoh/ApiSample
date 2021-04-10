9ff127cc8d7a6a843d5e4afbbd6b2d4a7b1b290a
Keoh <weipien.keoh@gmail.com>
2021-04-10 22:35:47 +0800

    initial commit

---
+++ Models/ToDoContext.cs
@@ -0,0 +1,15 @@
+﻿using API_Sample.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
